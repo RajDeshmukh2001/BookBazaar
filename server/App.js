776523476dc7a0b1fba1d5f79c020314cc2dc303
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2023-06-01 15:10:37 +0530

    Update App.js

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const router = require('./routes/book-routes');
 const cors = require('cors');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -12,5 +13,5 @@ mongoose.connect(
     'mongodb+srv://rajdd201:lMkrL8Cx1TwNVkIq@cluster0.8f8l1ue.mongodb.net/Book-Store?retryWrites=true&w=majority'
 ).then(() => console.log('Connected')
 ).then(() => {
-    app.listen(5000);
+    app.listen(PORT);
 }).catch((err) => console.log(err));
